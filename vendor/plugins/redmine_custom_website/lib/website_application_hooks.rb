c653cd44be72be20dfe1f26ac77fcd9eaaf85868
Jack Hong <jackhongx@gmail.com>
2010-01-21 11:59:39 +1100

    Braid: Updated mirror 'vendor/plugins/redmine_custom_website' to 'df9b0b9'

--- vendor/plugins/redmine_custom_website/lib/website_application_hooks.rb
+++ vendor/plugins/redmine_custom_website/lib/website_application_hooks.rb
@@ -1,7 +1,7 @@
 require 'redmine'
 
 class WebsiteApplicationHooks < Redmine::Hook::ViewListener
-  def view_layouts_base_content(context = {})
+  def view_layouts_base_banner(context = {})
     begin
       f = File.open(FileUtils.pwd + '/vendor/plugins/redmine_custom_website/assets/views/banner.rhtml', 'r')
       return f.read
